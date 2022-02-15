14ff46150f30a90f6be185c6f4275b06664449d9
TMPxyz <TMPxyz@gmail.com>
2022-02-15 12:12:49 +0800

    first commit

---
+++ bimap.py
@@ -0,0 +1,154 @@
+# bidi multimap
+
+from collections import defaultdict
+
+class MMap:
+    "multimap"
+    def __init__(self):
+        self._m = defaultdict(set)
+        self._pair_cnt = 0
+
+    def __contains__(self, k):
+        "if a key in mmap"
+        return k in self._m
+    
+    def __getitem__(self, k):
+        if k not in self._m: 
+            raise KeyError(f"unknown key {k}")
+        return frozenset(self._m[k])
+
+    def __len__(self):
+        return len(self._m)
+
+    def pair_count(self):
+        return self._pair_cnt
+
+    def keys(self):
+        return self._m.keys()
+
+    def values(self):
+        return self._m.values()
+
+    def items(self):
+        return self._m.items()
+
+    def iter_all_pairs(self):
+        for k, s in self._m.items():
+            for v in s:
+                yield k, v
+
+    def clear(self):
+        self._m.clear()
+        self._pair_cnt = 0
+
+    def has_pair(self, k, v):
+        s = self._m.get(k, None)
+        if not s: return False
+        return v in s
+        
+    def add_pair(self, k, v):
+        "add a pair k,v to mmap"
+        self._m[k].add(v)
+        self._pair_cnt += 1
+
+    def pop_pair(self, k, v):
+        "if k,v pair not present, raise KeyError"
+        if k not in self._m:
+            raise KeyError(f"unknown key {k}")
+        if v not in self._m[k]:
+            raise KeyError(f"unknown value {v}")
+        self._m[k].remove(v)
+        self._pair_cnt -= 1
+
+    def pop_all(self, k, d=None):
+        """remove all pairs under given key, return the value
+        if k not found, return d or KeyError if d is None
+        """
+        if k not in self._m:
+            if d is None:
+                raise KeyError(f"unknown key {k}")
+            else:
+                return d
+
+        s = self._m.pop(k)
+        self._pair_cnt -= len(s)
+        return s
+
+class BiMMap:
+    """a bidirectional multimap"""
+    def __init__(self):
+        self._m = MMap()
+        self._im = MMap()
+
+    def pair_count(self):
+        return self._m.pair_count()
+
+    def keys(self):
+        return self._m.keys()
+    def rkeys(self):
+        return self._im.keys()
+
+    def values(self):
+        return self._m.values()
+    def rvalues(self):
+        return self._im.values()
+
+    def items(self):
+        return self._m.items()
+    def ritems(self):
+        return self._im.items()
+
+    def clear(self):
+        self._m.clear()
+        self._im.clear()
+
+    def get(self, k, d=None):
+        if d is None:
+            return self._m[k]
+        else:
+            return self._m[k] if k in self._m else d
+    def rget(self, k, d=None):
+        if d is None:
+            return self._im[k]
+        else:
+            return self._im[k] if k in self._im else d
+
+    def iter_all_pairs(self):
+        return self._m.iter_all_pairs()
+    def riter_all_pairs(self):
+        return self._im.iter_all_pairs()
+
+    def has_pair(self, k, v):
+        return self._m.has_pair(k, v)
+    def rhas_pair(self, k, v):
+        return self._im.has_pair(k, v)
+
+    def add_pair(self, k, v):
+        self._m.add_pair(k, v)
+        self._im.add_pair(v, k)
+    def radd_pair(self, k, v):
+        self._im.add_pair(k, v)
+        self._m.add_pair(v, k)
+
+    def pop_pair(self, k, v):
+        self._m.pop_pair(k, v)
+        self._im.pop_pair(v, k)
+    def rpop_pair(self, k, v):
+        self._im.pop_pair(k, v)
+        self._m.pop_pair(v, k)
+
+    def pop_all(self, k):
+        s = self._m.pop_all(k)
+        for v in s:
+            self._im.pop_pair(v, k)
+    def rpop_all(self, k):
+        s = self._im.pop_all(k)
+        for v in s:
+            self._m.pop_pair(v, k)
+    
+    
+
+    
+
+    
+    
---
+++ bimap_unittest.py
@@ -0,0 +1,59 @@
+import unittest
+
+from bimap import BiMMap
+
+class BiMMapTest(unittest.TestCase):
+
+    def test_func(self):
+        d = BiMMap()
+        src = [('A', {1,2,3}), ('B', {2,3,4}), ('C', {1,2,4})]
+        rsrc = [(1, {'A', 'C'}), (2, {'A', 'B', 'C'}), (3, {'A', 'B'}), (4, {'B', 'C'})]
+
+        for k, vs in src:
+            for v in vs:
+                d.add_pair(k, v)
+        
+        self.assertEqual(d.pair_count(), 9)
+        self.assertCountEqual(d.keys(), ['A', 'B', 'C'])
+        self.assertCountEqual(d.rkeys(), [1,2,3,4])
+        self.assertCountEqual(d.values(), [{1,2,3}, {2,3,4}, {1,2,4}])
+        self.assertCountEqual(d.rvalues(), [{'A', 'C'}, {'A', 'B', 'C'}, {'A', 'B'}, {'B', 'C'}])
+        self.assertCountEqual(d.items(), src)
+        self.assertCountEqual(d.ritems(), rsrc)
+
+        self.assertCountEqual(d.iter_all_pairs(), [
+            ('A',1),('A',2),('A',3),('B', 2),('B', 3),('B', 4), ('C',1), ('C',2), ('C',4)
+        ])
+        self.assertCountEqual(d.riter_all_pairs(), [
+            (1,'A'),(1,'C'),(2,'A'),(2,'B'),(2,'C'),(3,'A'),(3,'B'),(4,'B'),(4,'C')
+        ])
+
+        self.assertTrue(d.has_pair('B', 4))
+        self.assertFalse(d.has_pair('B', 1))
+        self.assertTrue(d.rhas_pair(3, 'A'))
+        self.assertFalse(d.rhas_pair(3, 'C'))
+
+        self.assertRaises(KeyError, d.get, 'D')
+        self.assertRaises(KeyError, d.rget, 5)
+        self.assertRaises(KeyError, d.pop_all, 'D')
+        self.assertRaises(KeyError, d.rpop_all, 0)
+        self.assertRaises(KeyError, d.pop_pair, 'A', 4)
+        self.assertRaises(KeyError, d.rpop_pair, '2', 'D')
+
+        self.assertEqual(d.get('D', -1), -1)
+        self.assertEqual(d.get('A'), {1,2,3})
+        self.assertEqual(d.rget(100, -1), -1)
+        self.assertEqual(d.rget(4), {'B', 'C'})
+
+        d.pop_all('A')
+        self.assertEqual(d.pair_count(), 6)
+        d.rpop_all(1)
+        self.assertEqual(d.pair_count(), 5)
+        d.pop_pair('B', 2)
+        self.assertEqual(d.pair_count(), 4)
+        d.rpop_pair(2, 'C')
+        self.assertEqual(d.pair_count(), 3)
+
+        d.clear()
+        self.assertEqual(d.pair_count(), 0)
+        self.assertFalse(d.has_pair('C', 4))
